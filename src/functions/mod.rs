6e6fad91528d4aad8adf6395d9671146a957c511
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-05-09 18:59:40 -0700

    erroring as expected

---
+++ src/functions/mod.rs
@@ -0,0 +1,67 @@
+use anyhow::Result;
+use deno_core::op;
+use deno_core::Extension;
+use deno_core::JsRuntime;
+use deno_core::RuntimeOptions;
+use std::convert::TryFrom;
+use std::task::Context;
+use tokio::runtime::Handle;
+
+// fn to do an http get
+#[op]
+async fn op_http_get(url: String) -> Result<String, deno_core::error::AnyError> {
+    println!("http_get({})", url);
+
+    let body = reqwest::get(url).await?.text().await?;
+    println!("got body in rust {}", body);
+
+    Ok(body)
+}
+
+pub async fn execute_js() -> Result<()> {
+    println!("JS code execution started at {}", chrono::Local::now());
+
+    // Build a deno_core::Extension providing custom ops
+    let ext = Extension::builder()
+        .ops(vec![
+            // An op for getting an HTTP url
+            // The op-layer automatically deserializes inputs
+            // and serializes the returned Result & value
+            op_http_get::decl(),
+        ])
+        .build();
+
+    // Initialize a runtime instance
+    let mut js_runtime = JsRuntime::new(RuntimeOptions {
+        extensions: vec![ext],
+        ..Default::default()
+    });
+
+    let code_to_run = r#"
+      const console = {
+        log: function (value) {
+          Deno.core.print(value.toString() + "\n");
+        },
+      };
+      
+      const go = async () => {
+        const resp = await Deno.core.opAsync(
+          "op_http_get",
+          "https://ipfs.litgateway.com/ipfs/QmNiDrDnTiSo4y78qKwaZboq8KfT9Y3CRrnM7pfUmG1EFq"
+        );
+        console.log("resp: " + resp);
+      };
+      
+      go();
+    "#;
+
+    js_runtime
+        .execute_script("serverless_function.js", &code_to_run)
+        .unwrap();
+
+    js_runtime.run_event_loop(false).await;
+
+    println!("JS code execution finished at {}", chrono::Local::now());
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+#![allow(unused_variables)]
+
+#[macro_use]
+extern crate rocket;
+
+use rocket::http::Method;
+use rocket::http::Status;
+use rocket::response::status;
+use rocket::serde::json::{json, Value};
+use rocket_cors::{AllowedOrigins, CorsOptions};
+
+pub mod functions;
+
+/*
+curl --request POST http://localhost:8000/
+*/
+#[post("/")]
+async fn execute_js_function() -> status::Custom<Value> {
+    let resp = functions::execute_js();
+    match resp.await {
+        Ok(result) => return status::Custom(Status::Ok, json!({"result": true, "error": ""})),
+        Err(err) => {
+            return status::Custom(
+                Status::BadRequest,
+                json!({"message": "Execution error", "errorCode": "execution_error"}),
+            );
+        }
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .allowed_methods(
+            vec![Method::Get, Method::Post, Method::Patch]
+                .into_iter()
+                .map(From::from)
+                .collect(),
+        )
+        .allow_credentials(true)
+        .to_cors()
+        .expect("CORS failed to build");
+
+    rocket::build()
+        .mount("/", routes![execute_js_function])
+        .attach(cors)
+}
